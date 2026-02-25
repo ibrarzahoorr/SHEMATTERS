a78fdfa51ad5044cff507cd24f07632d025d2793
ibrarzahoorr <ibrarzahoor0@gmail.com>
2026-02-25 05:52:39 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const ProtectedRoute = ({ children }) => {
 
 function App() {
     return (
-        <Router>
+        <Router basename="/SHEMATTERS/">
             <ScrollToTop />
             <div className="flex flex-col min-h-screen">
                 <Navbar />
